c4b9425e3b9313cb8a8af086934fc0b3d2a512e5
G.Hemanth Tigga <99483175+HemanthTigga@users.noreply.github.com>
2023-01-05 00:34:38 +0530

    files updated

---
+++ Stacks/next_smaller_element.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+void nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    for(int i=n-1;i>=0;i--)
+    {
+        int curr = arr[i];
+        while(s.top()>=curr)
+        {
+            s.pop();
+        }
+        //ans is stack ka top
+        ans[i]=s.top();
+        s.push(curr);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    //return ans;
+}
+int main()
+{
+    vector<int> arr={2,1,4,3};
+    cout<<"hi"<<endl;
+    nextSmallerElement(arr,arr.size());
+    return 0;
+}
